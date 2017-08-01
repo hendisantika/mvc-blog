da49da4a34df735e377bdb9613920249448356f8
Hendi Santika <hendisantika@gmail.com>
2017-08-01 17:12:45 +0700

    #20 - Added Login Form model

---
+++ src/main/java/com/hendisantika/blog/form/LoginForm.java
@@ -0,0 +1,38 @@
+package com.hendisantika.blog.form;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/1/17
+ * Time: 5:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginForm {
+    @Size(min=2, max=30, message = "Username size should be in the range [2...30]")
+    private String username;
+
+    @NotNull
+    @Size(min=1, max=50)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
