34f96b3d87cd0589210c0087261e17f662920550
Hendi Santika <hendisantika@gmail.com>
2017-08-01 16:50:35 +0700

    #12 - Added Index Controller

--- src/main/java/com/hendisantika/blog/controller/HomeController.java
+++ src/main/java/com/hendisantika/blog/controller/HomeController.java
@@ -1,7 +1,14 @@
 package com.hendisantika.blog.controller;
 
+import com.hendisantika.blog.model.Post;
+import com.hendisantika.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,8 +22,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private PostService postService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Post> latest5Posts = postService.findLatest5();
+        model.addAttribute("latest5posts", latest5Posts);
+        List<Post> latest3Posts = latest5Posts.stream()
+                .limit(3).collect(Collectors.toList());
+        model.addAttribute("latest3posts", latest3Posts);
         return "index";
     }
+
+
 }
