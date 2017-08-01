8e0a10ce3e92da8b7e39b797356a5dda8ffb012d
Hendi Santika <hendisantika@gmail.com>
2017-08-01 17:07:00 +0700

    #17 - Added Posts Controller

--- src/main/java/com/hendisantika/blog/controller/PostsController.java
+++ src/main/java/com/hendisantika/blog/controller/PostsController.java
@@ -1,6 +1,7 @@
 package com.hendisantika.blog.controller;
 
 import com.hendisantika.blog.model.Post;
+import com.hendisantika.blog.service.NotificationService;
 import com.hendisantika.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,9 +24,17 @@ public class PostsController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private NotificationService notifyService;
+
     @RequestMapping("/posts/view/{id}")
     public String view(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
+
+        if (post == null) {
+            notifyService.addErrorMessage("Cannot find post #" + id);
+            return "redirect:/";
+        }
         model.addAttribute("post", post);
         return "posts/view";
     }
