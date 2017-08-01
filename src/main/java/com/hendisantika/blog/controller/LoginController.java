384ed97804a3582b5509b3fc6bf1fbd0703469a0
Hendi Santika <hendisantika@gmail.com>
2017-08-01 17:14:45 +0700

    #22 - Added Login Controller

---
+++ src/main/java/com/hendisantika/blog/controller/LoginController.java
@@ -0,0 +1,53 @@
+package com.hendisantika.blog.controller;
+
+import com.hendisantika.blog.form.LoginForm;
+import com.hendisantika.blog.service.NotificationService;
+import com.hendisantika.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/1/17
+ * Time: 5:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class LoginController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private NotificationService notifyService;
+
+    @RequestMapping("/users/login")
+    public String login(LoginForm loginForm) {
+        return "users/login";
+    }
+
+    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
+    public String loginPage(@Valid LoginForm loginForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            notifyService.addErrorMessage("Please fill the form correctly!");
+            return "users/login";
+        }
+
+        if (!userService.authenticate(
+                loginForm.getUsername(), loginForm.getPassword())) {
+            notifyService.addErrorMessage("Invalid login!");
+            return "users/login";
+        }
+
+        notifyService.addInfoMessage("Login successful");
+        return "redirect:/";
+    }
+}
