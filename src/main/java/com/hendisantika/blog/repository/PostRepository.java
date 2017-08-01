708d937f15834f375911b36ff0fbef89798a1fa6
Hendi Santika <hendisantika@gmail.com>
2017-08-01 18:14:50 +0700

    #24 - Modified PostRepository

--- src/main/java/com/hendisantika/blog/repository/PostRepository.java
+++ src/main/java/com/hendisantika/blog/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.hendisantika.blog.repository;
 
 import com.hendisantika.blog.model.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,4 +19,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.author ORDER BY p.date DESC")
+    List<Post> findLatest5Posts(Pageable pageable);
 }
